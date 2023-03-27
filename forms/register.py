563d1d28c89386edfe9cad7160fdcea9f921cd09
Nagib227 <artomak07@gmail.com>
2023-03-27 14:17:58 +0300

    add regoster form

---
+++ forms/register.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, BooleanField, SubmitField, EmailField
+from wtforms.validators import DataRequired
+
+
+class RegisterUserForm(FlaskForm):
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    name = StringField('Имя', validators=[DataRequired()])
+    age = StringField('Возраст', validators=[DataRequired()])
+    num_phone = StringField('Номер телефона', validators=[DataRequired()])
+    address = StringField('Адрес', validators=[DataRequired()])
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from data import db_session
 
 from flask import *
 from flask_restful import reqparse, abort, Api, Resource
+
+from forms.register import RegisterUserForm
  
 
 CUR_USER = None
@@ -31,6 +33,34 @@ def main():
 @app.route('/')
 def index():
     return render_template('index.html', link_logo=url_for('static', filename='img/logo.png'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterUserForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            age=int(form.age.data),
+            num_phone=form.num_phone.data,
+            address=form.address.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
